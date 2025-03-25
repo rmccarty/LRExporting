68c3a43680b6877836dc06d51241bb755deb1629
Ron McCarty <mccarty@yournetguard.com>
2025-03-25 13:36:20 -0500

    moved more config variables to config.py

--- config.py
+++ config.py
@@ -28,3 +28,60 @@ XML_NAMESPACES = {
     'photoshop': 'http://ns.adobe.com/photoshop/1.0/',
     'exif': 'http://ns.adobe.com/exif/1.0/'
 }
+
+# Exiftool configuration
+EXIFTOOL_BASE_ARGS = [
+    'exiftool',
+    '-overwrite_original',
+    '-handler=mdta'
+]
+
+# Metadata field mappings
+METADATA_FIELDS = {
+    'title': ['-Title', '-DisplayName', '-ItemList:Title'],
+    'date': [
+        '-CreateDate',
+        '-ModifyDate',
+        '-TrackCreateDate',
+        '-TrackModifyDate',
+        '-MediaCreateDate',
+        '-MediaModifyDate'
+    ],
+    'location': ['-LocationName'],
+    'city': ['-City'],
+    'state': ['-State'],
+    'country': ['-Country'],
+    'gps': ['-GPSLatitude', '-GPSLongitude'],
+    'caption': ['-Description']
+}
+
+# Verification fields to check
+VERIFY_FIELDS = [
+    '-DisplayName',
+    '-Title',
+    '-ItemList:Title',
+    '-Keywords',
+    '-Subject',
+    '-CreateDate',
+    '-ModifyDate',
+    '-TrackCreateDate',
+    '-TrackModifyDate',
+    '-MediaCreateDate',
+    '-MediaModifyDate',
+    '-LocationName',
+    '-City',
+    '-State',
+    '-Country',
+    '-GPSLatitude',
+    '-GPSLongitude',
+    '-Description'
+]
+
+# File naming configuration
+FILENAME_REPLACEMENTS = {
+    ':': ' -',
+    '/': '_'
+}
+LRE_SUFFIX = '__LRE'
+MCCARTYS_PREFIX = 'The McCartys '
+MCCARTYS_REPLACEMENT = 'The McCartys: '
